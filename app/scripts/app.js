fb798a80397097f8f101c575ccab2bbc5f677ca9
Igor Gorlov <gorlov35@gmail.com>
2016-07-22 03:45:41 +0300

    add range slider

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,6 @@ $(() => {
 	svg4everybody();
 
 	// Responsive menu
-
 	function menuResponsive() {
 		const menuResponsiveButton = $('.menu-responsive__button');
 		const menuResponsiveButtonActive = 'menu-responsive__button_active';
@@ -33,7 +32,6 @@ $(() => {
 
 
 	// Responsive menu
-
 	function menuOffcanvas() {
 		const menuOffcanvasButton = $('.menu-offcanvas__button');
 		const menuOffcanvasButtonActive = 'menu-offcanvas__button_active';
@@ -48,7 +46,6 @@ $(() => {
 	}
 
 	// Tabs
-
 	function tabs() {
 		const tabsNavLink = $('.tabs__link');
 		const tabsNavLinkActive = 'tabs__link_active';
@@ -70,19 +67,26 @@ $(() => {
 		});
 	}
 
-
+	// owlCarousel
 	function carouselOwl() {
 		const owlcarousel = $('.carousel-owl__list');
 		owlcarousel.owlCarousel({
-			margin: 30,
+			margin: 10,
+			autoWidth: false,
 			items: 3,
 			loop: true,
-			center: true,
-			stagePadding: 10,
+			center: false,
+			stagePadding: 0,
 			dots: true,
 			responsive: {
+				320: {
+					items: 2
+				},
 				768: {
 					items: 2
+				},
+				1000:{
+					items: 4
 				}
 			}
 
@@ -90,7 +94,7 @@ $(() => {
 		console.log('owlCarouselLoaded');
 	}
 
-
+	// bxSlider
 	function bxslider() {
 		const sliderBx = $('.slider-bx__list');
 		sliderBx.bxSlider({
@@ -106,6 +110,7 @@ $(() => {
 
 	}
 
+	// Spinner
 	function spinner() {
 		const amountSpinner = $('.spinner__input');
 		const amountSpinnerUp = $('.spinner__button_up');
@@ -128,13 +133,58 @@ $(() => {
 	}
 
 
+	function priceRange() {
+		const rangeSlider = $('.range-slider__slider');
+		const priceTextFrom = $('.range-slider__input#price-from');
+		const priceTextTo = $('.range-slider__input#price-to');
+		const priceInputFrom = $('.range-slider__price-from span');
+		const priceInputTo = $('.range-slider__price-to span');
+
+		// Range slider
+		rangeSlider.slider({
+			min: 100,
+			max: 99999,
+			values: [ 0, 99999 ],
+			range: true,
+			step: 1,
+			slide: function (event, ui) {
+				priceTextFrom.val( ui.values[0].toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1 ') + '');
+				priceTextTo.val( ui.values[1].toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1 ') + '');
+
+				priceInputFrom.text(ui.values[0]);
+				priceInputTo.text(ui.values[1]);
+
+				// $('.filter__price-input#price-from').val(ui.values[0]);
+				// $('.filter__price-input#price-to').val(ui.values[1]);
+			}
+			// change: function(event, ui) {
+			// 	$('.filter__price-input#price-from').val(ui.values[0]);
+			// 	$('.filter__price-input#price-to').val(ui.values[1]);
+			// }
+		});
+
+		// $('.filter__price-input#price-from').val($('.filter__range-slider').slider('values', 0).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + "");
+		// $('.filter__price-input#price-to').val($('.filter__range-slider').slider('values', 1).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + "");
+
+
+		// $('.range-slider__input#price-from').keyup(function () {
+		// 	$('.range-slider__slider').slider('values', 0, this.value);
+		// });
+		// $('.range-slider__input#price-to').keyup(function () {
+		// 	$('.range-slider__slider').slider('values', 0, this.value);
+		// });
+
+		console.log('rangeSlider loaded');
+	}
+
+
 	menuResponsive();
 	menuOffcanvas();
 	tabs();
 	carouselOwl();
 	bxslider();
 	spinner();
-
+	priceRange();
 
 
 });
